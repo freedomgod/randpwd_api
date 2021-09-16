a854996ba408128df8ec7ea3e530fc7276c3822d
freedomgod <wwhfree@qq.com>
2021-09-16 23:20:19 +0800

    test

--- api/index.py
+++ api/index.py
@@ -64,40 +64,41 @@ def rand_pin(para: dict) -> list:
 
 # 'index'是接口路径，methods不写，默认get请求
 # get方式访问
-# @app.route('/', methods=['get', 'post'])
-# def rand_pwd():
-#     # url参数格式：? l=20 & s=1 & c=1 & k=free
-#     # l 表示随机数的长度
-#     # s 表示是否包括标点符号等其他字符
-#     # c 表示字母是否有大写
-#     # k 表示是否包含关键字
-#     # n 表示生成的个数
-#
-#     l = request.args.get('l', 15)
-#     s = request.args.get('s', 0)
-#     c = request.args.get('c', 1)
-#     k = request.args.getlist('k')
-#     n = request.args.get('n', 1)
-#     para = {
-#         'length': int(l),
-#         'special': int(s),
-#         'capital': int(c),
-#         'key': k,
-#         'n': int(n)
-#     }
-#     res = {
-#         'pwd': rand_pin(para),
-#         'status': 200
-#     }
-#     return json.dumps(res, ensure_ascii=False)
+@app.route('/', methods=['get', 'post'])
+@app.route('/<path:path>')
+def rand_pwd(path):
+    # url参数格式：? l=20 & s=1 & c=1 & k=free
+    # l 表示随机数的长度
+    # s 表示是否包括标点符号等其他字符
+    # c 表示字母是否有大写
+    # k 表示是否包含关键字
+    # n 表示生成的个数
+
+    l = request.args.get('l', 15)
+    s = request.args.get('s', 0)
+    c = request.args.get('c', 1)
+    k = request.args.getlist('k')
+    n = request.args.get('n', 1)
+    para = {
+        'length': int(l),
+        'special': int(s),
+        'capital': int(c),
+        'key': k,
+        'n': int(n)
+    }
+    res = {
+        'pwd': rand_pin(para),
+        'status': 200
+    }
+    return json.dumps(res, ensure_ascii=False)
 
 
-@app.route('/')
-# @app.route('/<path:path>')
-def catch_all(path):
-    print(request.args)
-    key = request.args.get('key')
-    return Response("<h1>Flask</h1><p>You visited: /%s</p><p>key=%s</p>" % (path, key), mimetype="text/html")
+# @app.route('/')
+# # @app.route('/<path:path>')
+# def catch_all(path):
+#     print(request.args)
+#     key = request.args.get('key')
+#     return Response("<h1>Flask</h1><p>You visited: /%s</p><p>key=%s</p>" % (path, key), mimetype="text/html")
 
 
 if __name__ == "__main__":
