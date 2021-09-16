0cfed35fb29b2cfa621356b2c7ab0bfc5c44d093
freedomgod <wwhfree@qq.com>
2021-09-16 16:30:59 +0800

    init commit

---
+++ index.py
@@ -0,0 +1,40 @@
+import flask
+import json
+from util import rand_pin
+
+# 实例化api，把当前这个python文件当作一个服务，__name__代表当前这个python文件
+api = flask.Flask(__name__)
+
+
+# 'index'是接口路径，methods不写，默认get请求
+@api.route('/', methods=['get', 'post'])
+# get方式访问
+def all_rand():
+    # url参数格式：? l=20 & s=1 & c=1 & k=free
+    # l 表示随机数的长度
+    # s 表示是否包括标点符号等其他字符
+    # c 表示字母是否有大写
+    # k 表示是否包含关键字
+    # n 表示生成的个数
+
+    l = flask.request.args.get('l', 15)
+    s = flask.request.args.get('s', 0)
+    c = flask.request.args.get('c', 1)
+    k = flask.request.args.getlist('k')
+    n = flask.request.args.get('n', 1)
+    para = {
+        'length': int(l),
+        'special': int(s),
+        'capital': int(c),
+        'key': k,
+        'n': int(n)
+    }
+    res = {
+        'pwd': rand_pin(para),
+        'status': 200
+    }
+    return json.dumps(res, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    api.run(port=8888, debug=True, host='127.0.0.1')
