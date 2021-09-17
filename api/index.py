7eee28541106e98e30544d0833ba874241480a97
freedomgod <wwhfree@qq.com>
2021-09-17 09:31:40 +0800

    add README.md and update index.py

--- api/index.py
+++ api/index.py
@@ -1,64 +1,10 @@
-# import flask
-from flask import Flask, Response, request
+from flask import Flask, request
 import json
 from gevent import pywsgi
-# from util import rand_pin
 from api.util import rand_pin
 
 # 实例化api，把当前这个python文件当作一个服务，__name__代表当前这个python文件
 app = Flask(__name__)
-
-
-# def rand_pin(para: dict) -> list:
-#     """
-#     依据参数生成随机字符串
-#     param length: 字符串长度
-#     param special: 是否包含特殊的字符串，可选值有0, 1, 2, 3，分别表示不包含、包含特殊字符、包含标点、包含特殊字符和标点，默认为0
-#     param capt: 字母大写的个数
-#     param key: 包含关键字
-#     :return: 字符串
-#     """
-#     digits = list('0123456789')  # 数字
-#     alph = list('abcdefghijklmnopqrstuvwxyz')  # 字母
-#     dots = list(',./?;:\'"[]{}\\|')  # 标点
-#     op = list('!@#$%^&*()_+=-~`')  # 特殊符号
-#     st_pool = digits + alph  # 基础字符池
-#
-#     special = para.get('special')
-#     length = para.get('length')
-#     capt = para.get('capital')
-#     key = para.get('key')
-#     n = para.get('n')
-#
-#     if special == 0:  # 添加额外的字符
-#         pass
-#     elif special == 1:
-#         st_pool += op
-#     elif special == 2:
-#         st_pool += dots
-#     else:
-#         st_pool += dots + op
-#
-#     pin = []
-#     for i in range(n):
-#         pin_lis = []
-#         if key is None:
-#             if capt >= length:
-#                 pin_lis += [random.choice(st_pool).upper() for _ in range(length)]
-#             else:
-#                 capt_st = [random.choice(string.ascii_uppercase) for _ in range(capt)]
-#                 pin_lis += [random.choice(st_pool) for _ in range(length - capt)]
-#                 pin_lis += capt_st
-#         else:
-#             if (len(''.join(list(key))) + capt) >= length:
-#                 pin_lis += list(key)
-#             else:
-#                 capt_st = [random.choice(string.ascii_uppercase) for _ in range(capt)]
-#                 pin_lis += [random.choice(st_pool) for _ in range(length - len(''.join(key)) - capt)] + capt_st + key
-#         random.shuffle(pin_lis)
-#         tmp = ''.join(pin_lis)
-#         pin.append(tmp)
-#     return pin
 
 
 # 'index'是接口路径，methods不写，默认get请求
